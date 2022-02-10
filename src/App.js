d0fd6f2cf21275ba56614cdedc5072fc39d0144b
Vikash kumar <vikashkumar17r@gmal.com>
2022-02-10 23:17:10 +0530

    gh-pages -d build

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
                 <SquareComponent onClick={() => onUserClicked(8)} state={gameState[8]}/>
             </div>
             <button className="clear-button" onClick={clearGame}>Clear Game</button>
-            <p className="fc-aqua fw-600" style={{color:"lightPink", fontSize:"20px"}}add><i>By Vikash Kumar</i></p>
+            <p className="fc-aqua fw-600"><i>By Vikash Kumar</i></p>
         </div>
     );
 }
